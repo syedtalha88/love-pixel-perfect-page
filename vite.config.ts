dd1a6253d4414483e31fe26a48cb17ab2833b041
syedtalha88 <talhasyed910@gmail.com>
2025-08-27 13:57:10 +0530

    netlify changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base:"/love-pixel-perfect-page",
+  base:"/",
   server: {
     host: "::",
     port: 8080,
